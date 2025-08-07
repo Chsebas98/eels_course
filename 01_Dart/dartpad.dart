d50ecf8968d3cffe1dacfb1ecf0ab0e6ca7c5e52
Sebastian Soberon <sebastian.soberon@securitydata.net.ec>
2025-08-06 20:20:31 -0500

    feat: variables basicas

--- 01_Dart/dartpad.dart
+++ 01_Dart/dartpad.dart
@@ -1,3 +1,16 @@
 void main() {
-  print('Hola mundo');
+  //Saludo
+  final String saludo = "Hola mundo";
+  print(saludo);
+  //int
+  final int edad = 26;
+  print("Mi edad es: $edad");
+  //double
+  final double money = 205.45874;
+  print("Money restante: $money");
+
+  //Strings
+  final name = "Sebastian";
+  final ape = "Soberon";
+  print("$name $ape");
 }
